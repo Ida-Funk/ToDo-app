f31a1ad5156f0b05ba21194b680221e52ffa87a7
Ida Funk <ida.funk@hotmail.com>
2025-09-10 12:26:13 +0200

    initial commit

---
+++ todo/api-service/Models/fileToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo.api_service.Models
+{
+    class fileToDo
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ todo/api-service/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using todo.api_service.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<FileService>();
+
+var app = builder.Build();
+
+app.MapGet("/app/todo", (FileService fileService) =>
+{
+    var value = fileService.ReadFromFile();
+
+    return results.ok(value);
+});
+
+//app.MapPost("/app/todo", (FileService fileService, string content) =>
+//{
+//    fileService.WriteToFile(content);
+//    return Results.Ok("Content written to file successfully.");
+            //});
+
+app.Run();
